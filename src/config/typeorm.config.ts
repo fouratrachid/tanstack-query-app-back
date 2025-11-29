cf1dd380860945bfe9b35e2008f593048d32d0c1
fouratrachid <rachidfourat2@gmail.com>
2025-11-29 15:46:26 +0100

    feat: add TypeORM configuration for PostgreSQL database connection

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { User } from '../entities/user.entity';
+import { RefreshToken } from '../entities/refresh-token.entity';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: [User, RefreshToken],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+});
